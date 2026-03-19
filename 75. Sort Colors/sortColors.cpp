a05e943ea5471f0b1dbf47872c7700bc8a14215f
Ayush Singh <singhayush2041@gmail.com>
2026-03-19 18:45:56 +0530

    solve Qs

---
+++ 75. Sort Colors/sortColors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0,
+        j = nums.size()-1,
+        k = 0;
+
+        while(k<=j){
+            if(nums[k] == 1){
+                k++;
+            }else if(nums[k] == 2){
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                j--;
+            } else{
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
+                i++;
+                k++;
+            }
+        }
+
+    }
+};
