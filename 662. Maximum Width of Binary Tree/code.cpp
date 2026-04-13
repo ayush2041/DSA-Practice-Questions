bee311b7fe8e6aa8c5bdf88aaee1fcc7080c403a
Ayush Singh <singhayush2041@gmail.com>
2026-04-13 17:50:32 +0530

    solve Qs

---
+++ 662. Maximum Width of Binary Tree/code.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        int maxWidth = 0;
+
+        while(q.size()>0){
+            int currLevelSize = q.size();
+            int stIdx = q.front().second;
+            int endIdx = q.back().second;
+            
+            maxWidth = max(maxWidth, endIdx-stIdx+1);
+
+            for(int i=0; i<currLevelSize; i++){
+                auto curr = q.front();
+                q.pop();
+                if(curr.first->left){
+                    q.push({curr.first->left, curr.second*2+1});
+                }
+                if(curr.first->right){
+                    q.push({curr.first->right, curr.second*2+2});
+                }
+            }
+        }
+        return maxWidth;
+    }
+};
